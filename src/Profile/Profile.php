0166667cb5c1425991279fd84a7808396ec2642b
Urs Lang <urs.lang@luzern.com>
2026-08-13 11:03:22 +0200

    .

--- src/Profile/Profile.php
+++ src/Profile/Profile.php
@@ -2,7 +2,10 @@
 
 namespace LuzernTourismus\M365Mail\Profile;
 
-class Profile
+use LuzernTourismus\M365Mail\Graph\Reader\UsergroupItem;
+use Nemundo\Core\Base\AbstractBase;
+
+class Profile extends AbstractBase
 {
 
     public readonly string $email;
@@ -11,17 +14,50 @@ class Profile
 
     public readonly string $name;
 
+    private $profileJson;
+
 
     public function __construct($profileJson)
     {
 
+        $this->profileJson = $profileJson;
 
         $this->email = $profileJson['mail'];
         $this->login = $profileJson['userPrincipalName'];
         $this->name = $profileJson['displayName'];
 
+    }
+
+
+    public function isMemberOfUsergroup($usergroupId)
+    {
+
+        $value = false;
+        foreach ($this->getGroupMembershipList() as $usergroupItem) {
+
+            if ($usergroupItem->id === $usergroupId) {
+                $value = true;
+            }
+
+        }
+
+        return $value;
 
     }
 
+
+    /**
+     * @return UsergroupItem[]
+     */
+    public function getGroupMembershipList()
+    {
+
+        foreach ($this->profileJson['memberOf'] as $usergroupJson) {
+            $list[] = new UsergroupItem($usergroupJson);
+        }
+
+        return $list;
+
+    }
 
 }
--- src/Profile/ProfileRequest.php
+++ src/Profile/ProfileRequest.php
@@ -2,10 +2,13 @@
 
 namespace LuzernTourismus\M365Mail\Profile;
 
+use LuzernTourismus\M365Mail\Graph\Config\GraphConfig;
 use LuzernTourismus\M365Mail\Graph\Reader\UsergroupItem;
 use LuzernTourismus\M365Mail\Graph\Request\GraphRequest;
 use Nemundo\Core\Json\Reader\JsonReader;
+use Nemundo\Core\TextFile\Writer\TextFileWriter;
 use Nemundo\Core\WebRequest\BearerAuthentication\JsonBearerAuthenticationWebRequest;
+use Nemundo\Project\Path\TmpPath;
 
 class ProfileRequest
 {
@@ -15,52 +18,27 @@ class ProfileRequest
     public function getProfile()
     {
 
-        $url = 'https://graph.microsoft.com/v1.0/me';
+        $url = 'https://graph.microsoft.com/v1.0/me?$expand=transitiveMemberOf';
 
         $curl = new JsonBearerAuthenticationWebRequest();
         $curl->bearerAuthentication = $this->token;
         $response = $curl->getUrl($url);
 
+        if (GraphConfig::$debugMode) {
+
+            $filename = (new TmpPath())->addPath('profile.json')->getFullFilename();
+
+            $file = new TextFileWriter($filename);
+            $file->overwriteExistingFile = true;
+            $file->addLine($response->html);
+            $file->writeFile();
+
+        }
+
         $profileJson = (new JsonReader())->fromText($response->html)->getData();
         $profile = new Profile($profileJson);
 
         return $profile;
-
-    }
-
-
-    public function isMemberOfUsergroup($usergroupId)
-    {
-
-        $value = false;
-        foreach ($this->getGroupMembershipList() as $usergroupItem) {
-
-            if ($usergroupItem->id === $usergroupId) {
-                $value = true;
-            }
-
-        }
-
-        return $value;
-
-    }
-
-
-    /**
-     * @return UsergroupItem[]
-     */
-    public function getGroupMembershipList()
-    {
-
-        $endpoint = 'me/transitiveMemberOf/microsoft.graph.group?$select=id,displayName';
-
-        $list = [];
-        $data = (new GraphRequest())->getData($endpoint);
-        foreach ($data as $usergroupJson) {
-            $list[] = new UsergroupItem($usergroupJson);
-        }
-
-        return $list;
 
     }
 
